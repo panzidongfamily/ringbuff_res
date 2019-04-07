b167d1db2a3822d4ca618948dcf169c11df71a08
tilz0R <tilen.majerle@gmail.com>
2019-04-07 19:25:19 +0200

    Modify mainpage with license

--- docs/src/_mainpage.h
+++ docs/src/_mainpage.h
@@ -15,6 +15,7 @@
  *  - Interrupt safe when used as pipe with single write and single read entries
  *  - Suitable for DMA transfers from and to memory with zero-copy between buffer and application memory
  *  - Supports data peek, skip for read and advance for write
+ *  - User friendly MIT license
  *
  * \section         sect_resources Download & Resources
  *
